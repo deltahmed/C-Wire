004095db58977463ffd25e51cae6a4e735d43230
abdel8z <167315991+abdel8z@users.noreply.github.com>
2024-12-20 22:57:55 +0100

    Add files via upload

--- codeC/include/Station.h
+++ codeC/include/Station.h
@@ -10,9 +10,19 @@
 
 #ifndef Station_H
 #define Station_H
-#include "CWIRE_def.h"
-#include "CWIRE_error.h"
-#include "CWIRE_debug.h"
+//Necessary includes for definitions and utilities 
+#include "CWIRE_def.h" // Custom definitions for lint and related types
+#include "CWIRE_error.h" // Error handling utilities 
+#include "CWIRE_debug.h" // Debugging tools
+
+/**
+ * @struct station_struct
+ * @brief Represents an electricity station.
+ * This structure stores the essential data for an electricity station, including : 
+ * -"id" : A unique identifier for the station.
+ * -"capacity" : The maximum capacity of the station (in kWh).
+ * -"load" : The current load on the station (in kWh).
+ */
 
 typedef struct station_struct
 {
@@ -21,10 +31,15 @@ typedef struct station_struct
     lint load;
 }Station;
 
+/**
+ * @brief Creates a new Station instance. 
+ * @param id The unique identifier of the station.
+ * @param capacity The maximum capacity of the station.
+ * @param load The current load on the station.
+ * @return The newly created Station.
+ */
 Station CreateStation(lint id, lint capacity, lint load);
 
 #endif
-
-
 
 
